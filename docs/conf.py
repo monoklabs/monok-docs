83a2407a53bc0e75298e5a976447021d3f32cfc2
monokresearch <55885374+monokresearch@users.noreply.github.com>
2020-04-23 17:51:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -96,8 +96,7 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-# The 
-to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
 
@@ -119,11 +118,6 @@ html_theme = 'default'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None
-html_logo = 'img/logo.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
